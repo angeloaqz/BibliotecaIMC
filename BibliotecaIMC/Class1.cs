9a6581aba4fd0e4a499d907d74aafd357f04b08b
alunolab <alunolab@LAB-113-08>
2026-08-11 21:22:37 -0300

    Adicionar arquivos de projeto.

---
+++ BibliotecaIMC/Class1.cs
@@ -0,0 +1,10 @@
+﻿namespace BibliotecaIMC
+{
+    public class Calculo
+    {
+        public double CalcularIMC(double peso, double altura)
+        {
+            return peso / (altura * altura);
+        }
+    }
+}
---
+++ TesteBibliotecaIMC/Program.cs
@@ -0,0 +1,22 @@
+﻿using BibliotecaIMC;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Calculo calculo = new Calculo();
+
+        Console.Write("Digite seu peso em kg: ");
+        double peso = double.Parse(Console.ReadLine());
+
+        Console.Write("Digite sua altura em metros: ");
+        double altura = double.Parse(Console.ReadLine());
+
+        double imc = calculo.CalcularIMC(peso, altura);
+
+        Console.WriteLine();
+        Console.WriteLine($"Seu IMC é: {imc:F2}");
+
+        Console.ReadKey();
+    }
+}
